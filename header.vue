73b399202292c7efe96be4d314ae2693685575f2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-17 14:38:33 -0500

    East York Town Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -86,16 +86,6 @@
     							<div class="header_social">
     							    <social-links></social-links>
     							</div>
-    							<div>
-    							    <a href="https://play.google.com/store/apps/details?id=ca.wearecircus.bcc" target="_blank">
-    					                <p class="accessibility"></p>
-					                    <img class="app_badge" src="//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/png/1532627380000/googleplay.png" alt="Google Play Store" />
-					                </a>
-					                <a href="https://itunes.apple.com/ca/app/bramalea-city-centre/id1042094590?mt=8" target="_blank">
-    					                <p class="accessibility"></p>
-					                    <img class="app_badge" src="//codecloud.cdn.speedyrails.net/sites/5c0581a36e6f643f53050000/image/png/1532627498000/applestore.png" alt="Apple App Store" />
-					                </a>
-					            </div>
     							<div class="small_hr"></div>
     						</nav>
     					</transition>
