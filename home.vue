d90115873d9c60027a7ebe956baa7a5730e64d55
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-17 16:01:38 -0500

    East York Town Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -117,15 +117,15 @@
                       
                         if( _.includes([0,5], key) ) {
                             value.masonry_class = "grid-item--height2";
-                            // value.image_url = "https://picsum.photos/570/1140?image=98"+key;
+                            value.image_url = "https://picsum.photos/570/1140?image=98"+key;
                         }
                         else if ( _.includes([7], key) ){
                             value.masonry_class = "grid-item--width2";
-                            // value.image_url = "https://picsum.photos/1140/570?image=97"+key;
+                            value.image_url = "https://picsum.photos/1140/570?image=97"+key;
                         }
                         else {
                             value.masonry_class = " ";
-                            // value.image_url = "https://picsum.photos/570/570?image=88"+key;
+                            value.image_url = "https://picsum.photos/570/570?image=88"+key;
                         }
                         if(value.name === null || value.name === undefined || value.name.length == 0) {
                             value.no_hover_class = false;
