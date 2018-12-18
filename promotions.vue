1fc80005b66eea3a6ce56601823c531cb7d3c1d6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-18 12:42:17 -0500

    East York Town Centre 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -13,7 +13,7 @@
 		<div class="site_container page_content">
 			<div id="events_container" v-if="promotions.length > 0">
 				<!--<paginate name="promos" v-if="promos" :list="promos" class="paginate-list margin-60" :per="4">-->
-					<div class="row event_container" v-for="(promo,index) in paginated('promos')" :class="{ 'last': index === (paginated('promos').length - 1) }">
+					<div class="row event_container" v-if="index" v-for="(promo,index) in promos" :class="{ 'last': index === (paginated('promos').length - 1) }">
 						<div class="col-sm-6 col-md-4 event_image_container">
 							<img :src="promo.store.store_front_url_abs"  class="event_image image" :alt="'Click here to view ' + promo.name"/>
 						</div>
