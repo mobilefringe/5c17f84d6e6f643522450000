d26372de743b20ec1ae9e0766a4d0b254a3d51dd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-18 16:23:22 -0500

    East York Town Centre 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -104,8 +104,10 @@
                         
                         if(temp_repo && temp_repo.images) {
                             this.pageBanner = temp_repo.images[0];
+                        } else {
+                            this.pageBanner = {};
+                            this.pageBanner.image_url = ""
                         }
-                        this.pageBanner = this.pageBanner;
                     });
                 },
                 updateAccessibilityData() {
