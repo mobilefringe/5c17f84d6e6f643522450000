b53ad4fa8a610606c1e4af0d8ac016ffea7e9d09
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-18 12:53:01 -0500

    East York Town Centre 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -30,7 +30,7 @@
 						
 							<div class="text-right  col-sm-6" v-if="promo" style="padding:0">
 								<router-link :to="'/events/'+ promo.slug" class="event_learn_more pull-left hvr-icon-wobble-horizontal" :aria="promo.name">
-								    {{$t("events_page.read_more")}} <i class="fa fa-angle-right" aria-hidden="true"></i>
+								    {{$t("events_page.read_more")}} <i class="fa fa-angle-right hvr-icon" aria-hidden="true"></i>
 							    </router-link>
 								<social-sharing :url="shareURL(promo.slug)" :title="promo.title" :description="promo.body" :quote="_.truncate(promo.description, {'length': 99})" twitter-user="BCCstyle" :media="promo.image_url" inline-template >
 									<div class="blog-social-share pull_right">
