88dd2906a3f6279bebed128aa87deebc32bdbdc7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-18 16:33:36 -0500

    East York Town Centre 1.0  /thank_you.vue

---
+++ thank_you.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="page_container" id="thank_you_container">
+    <div class="row">
+        <!--<img src="http://placehold.it/800x300" class="" alt="">-->
+    </div>
+        <div class="row">
+            Your subscription has been confirmed. You've been added to our list and will hear from us soon.
+        </div>
+    </div>
+</template>
+<style>
+    #thank_you_container div{
+        padding: 20px;
+    }
+</style>
+<script>
+    define(["Vue"], function(Vue) {
+        return Vue.component("thank-you-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    title: "title"
+                }
+            }
+        });
+    });
+</script>
