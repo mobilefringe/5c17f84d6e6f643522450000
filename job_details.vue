9c4e93096de1cf1b0de2991858acd4b993b0abaa
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-18 16:31:05 -0500

    East York Town Centre 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="promo_dets_container" v-if="currentJob">
-		<div class="page_header" v-if="jobBanner" v-bind:style="{ backgroundImage: 'url(' + jobBanner.image_url + ')' }">
+		<div class="page_header" v-if="jobBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
 			<div class="site_container">
 				<div class="header_content caps">
 					<h1>{{$t("jobs_page.jobs")}}</h1>
