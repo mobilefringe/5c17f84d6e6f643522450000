2affbe85d7622b0b43481bbc22a7d1fd7769e68c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-19 12:54:34 -0500

    East York Town Centre 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -10,7 +10,7 @@
         <div class="site_container">
             <div class="row"> 
                 <div class="col-md-12 contact_contents">
-                    <form  class="form-horizontal js-cm-form" action="https://mobilefringe.createsend.com/t/d/s/vltqi/" method="post">
+                    <form  class="form-horizontal js-cm-form" action="https://mobilefringe.createsend.com/t/d/s/gmdyi/" method="post">
                     <!--id="subForm" action="form-submit" v-on:submit.prevent="validateBeforeSubmit"-->
                         <div class="form-group ">
                             <div class="col-sm-6 col-xs-12" >
@@ -21,7 +21,7 @@
                         <div class="form-group">
                             <div class="col-sm-6 col-xs-12">
                                 <label class="label" for="newsletter_email">{{$t("newsletter_page.email")}}</label>
-                                <input v-model="form_data.email" required class="form-control js-cm-email-input" name="cm-vltqi-vltqi" type="email" placeholder="Email" id="newsletter_email">
+                                <input v-model="form_data.email" required class="form-control js-cm-email-input" name="cm-gmdyi-gmdyi" type="email" placeholder="Email" id="newsletter_email">
                             </div>
                         </div>
                         <div class="form-group">
