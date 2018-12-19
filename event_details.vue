c57714b4d2ce083b3b6e0e276acafc32ca37e4b6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-19 12:17:06 -0500

    East York Town Centre 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="event_dets_container" v-if="currentEvent">
-		<div class="page_header" v-if="eventBanner" v-bind:style="{ backgroundImage: 'url(' + eventBanner.image_url + ')' }">
+		<div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + eventBanner.image_url + ')' }">
 			<div class="site_container">
 				<div class="header_content caps">
 					<h1>{{$t("events_page.events")}}</h1>
