cf4b55e84e583cfbeb5d0986beca586c38830c80
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-15 18:09:03 -0500

    East York Town Centre 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -238,6 +238,23 @@
                     this.map = map;
                      this.dropPin(this.currentStore);
                 },
+                checkImageURL(value) {
+      if (_.includes(value.image_url, "missing")) {
+        if (value.store === null || value.store === undefined) {
+          return "https://www.mallmaverick.com/system/site_images/photos/000/044/637/original/default_logo.png?1539618404";
+        } else if (
+          value.store != null &&
+          value.store != undefined &&
+          _.includes(value.store.store_front_url_abs, "missing")
+        ) {
+          return "https://www.mallmaverick.com/system/site_images/photos/000/044/637/original/default_logo.png?1539618404";
+        } else {
+          return value.store.store_front_url_abs;
+        }
+      } else {
+        return value.promo_image_url_abs;
+      }
+    }
             }
         });
     });
