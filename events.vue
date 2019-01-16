5923cf2e2f63f5f120ed81cd2fe07b54a096a091
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-16 14:29:25 -0500

    East York Town Centre 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -59,12 +59,6 @@
 					<p>{{$t("events_page.no_event_message")}}</p>
 				</div>
 			</div>
-			<!--<div class="row margin-60">-->
-			<!--	<div class="col-md-12">-->
-			<!--		<paginate-links for="events" :async="true" :limit="5" :show-step-links="true"></paginate-links>-->
-					<!--<paginate-links for="currentSelection" :async="true" :simple="{ next: 'Next »', prev: '« Back' }"></paginate-links>-->
-			<!--	</div>-->
-			<!--</div>-->
 			<div class="show_more"  v-if="events.length > 0">
               <div
                 class="pointer"
