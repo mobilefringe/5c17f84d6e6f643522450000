387b0d2f53922c62af9c15215ba805090d41d2c3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-18 13:51:40 -0500

    East York Town Centre 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -59,10 +59,9 @@
 					<p>{{$t("events_page.no_event_message")}}</p>
 				</div>
 			</div>
-			<div class="show_more" v-if="promos && showMore <= promos.length"
+			<div class="show_more"  v-if="promos && showMore <= promos.length">
               <div
                 class="pointer"
-                
                 @click="loadMoreItems()"
               >{{$t("events_page.more_promos")}}</div>
             </div>
