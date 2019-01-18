c1e2a71f276f7b8a766e57577cca3a49276fc911
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-18 16:20:02 -0500

    East York Town Centre 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -28,7 +28,7 @@
                                     <span v-if="locale=='en-ca'">{{hour.holiday_name}}</span>
                                     <span v-else>{{hour.holiday_name_2}}</span>
                                     ({{hour.holiday_date | moment("MMM D YYYY", timezone)}}) :
-                                    <span>{{hour.open_time | moment("h:mm A", timezone)}} - {{hour.close_time | moment("h:mm A", timezone)}}</span>
+                                    <span class="opening_hours">{{hour.open_time | moment("h:mm A", timezone)}} - {{hour.close_time | moment("h:mm A", timezone)}}</span>
                                 </span>
                             </div>
                         </div>
