fd953daf3549e07ee34593f47dd79606382c8453
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-02 13:48:41 -0400

    East York Town Centre 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
-        <a href="https://www.facebook.com/EastYorkTownCentre" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
-        <a href="https://www.twitter.com/EYTCshops" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
-         
+        <a href="https://www.facebook.com/EastYorkTownCentre" target="_blank">
+            <i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p>
+        </a>
+        <a href="https://www.twitter.com/EYTCshops" target="_blank">
+            <i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p>
+        </a>
+        <a href="https://www.instagram.com/eytcshops/" target="_blank">
+            <i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Instagram</p>
+        </a>
     </div>
-    
 </template>
 <style>
     .lb-data .lb-number, .lb-nav {
