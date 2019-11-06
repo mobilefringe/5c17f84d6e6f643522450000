1a64243021ad23f821683b5e97949dabbd1d6a37
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-06 15:22:44 -0500

    East York Town Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -75,7 +75,8 @@
                     meta: {
                         meta_title: "",
                         meta_description: "",
-                        meta_keywords: ""
+                        meta_keywords: "",
+                        meta_image: ""
                     }
                 }
             },
